27c7c7eb334a56067588de070f89324819c6b3dc
ijluciani <ijluciani@298503bc-7942-11de-bf68-bf76f4d3b5b5>
2009-07-27 19:38:30 +0000

    Se hace un refactor dentro de bowling. Se cambia la camara de lugar. Se implementa el uso de keys para jugar.

--- trunk/bowling/src/bowling/main/Bowling.java
+++ trunk/bowling/src/bowling/main/Bowling.java
@@ -13,18 +13,21 @@ import bowling.utils.ScToJme;
 
 import com.jme.bounding.BoundingBox;
 import com.jme.input.InputHandler;
+import com.jme.input.KeyInput;
+import com.jme.input.action.InputAction;
+import com.jme.input.action.InputActionEvent;
 import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
 import com.jme.renderer.ColorRGBA;
 import com.jme.renderer.Renderer;
 import com.jme.scene.Node;
 import com.jme.scene.Spatial;
+import com.jme.scene.Text;
 import com.jme.scene.shape.Sphere;
 import com.jme.scene.state.BlendState;
 import com.jme.scene.state.MaterialState;
 import com.jme.util.export.JMEImporter;
 import com.jme.util.export.binary.BinaryImporter;
-import com.jmetest.physics.Utils;
 import com.jmex.physics.DynamicPhysicsNode;
 import com.jmex.physics.PhysicsNode;
 import com.jmex.physics.StaticPhysicsNode;
@@ -35,9 +38,11 @@ import com.jmex.physics.util.SimplePhysicsGame;
 
 public class Bowling extends SimplePhysicsGame {
 
-	private InputHandler physicsStepInputHandler;
 	private DynamicPhysicsNode ball;
 	private List<DynamicPhysicsNode> pins;
+	//TODO: hacer una clase BowlingLine que contenga todo esto
+	private PhysicsBox lineFloor, rightGutter, leftGutter, backGutter, rightWall, leftWall, backWall;
+	private int forceMagnitude;
 	
 	final private static String RESOURCE_PATH = "resources" + File.separatorChar;
 	final private static String MODELS_PATH = RESOURCE_PATH + "models" + File.separatorChar;
@@ -98,102 +103,80 @@ public class Bowling extends SimplePhysicsGame {
 		
 		// TODO : Replace this for actual loading of the models
 		
-        // first we will create the floor
-        // as the floor can't move we create a _static_ physics node
-        StaticPhysicsNode staticNode = getPhysicsSpace().createStaticNode();
-
-        // attach the node to the root node to have it updated each frame
-        rootNode.attachChild( staticNode );
-
-        
-        //TODO: REFACTOR
-        
-        // now we create a collision geometry for the floor - a box
-        PhysicsBox floorBox = staticNode.createBox( "floor");
-
-        
-        // the box is already attached to our static node
-        // it currently has height, width and depth of 1
-        // resize it to be 10x10 thin (0.5) floor
-        floorBox.getLocalScale().set( 5, 1f, 70 );
-        //floorBox.setMaterial(Material.ICE);
-
-        PhysicsBox leftGutter = staticNode.createBox( "leftGutter" ); 
-        leftGutter.getLocalScale().set( 2, 0.5f, 70 );
-        leftGutter.setLocalTranslation(3.5f, -0.25f, 0);
-
-        PhysicsBox rightGutter = staticNode.createBox( "rightGutter" ); 
-        rightGutter.getLocalScale().set( 2, 0.5f, 70 );
-        rightGutter.setLocalTranslation(-3.5f, -0.25f, 0);
-
-        PhysicsBox backGutter = staticNode.createBox( "backGutter" ); 
-        backGutter.getLocalScale().set( 9, 0.5f, 5f );
-        backGutter.setLocalTranslation(0f, -0.25f, 37.5f);
-
-        PhysicsBox leftWall = staticNode.createBox( "leftWall" ); 
-        leftWall.getLocalScale().set(2f, 2f, 75f );
-        leftWall.setLocalTranslation(5.5f, 0.5f, 2.5f);
-
-        PhysicsBox rightWall = staticNode.createBox( "rightWall" ); 
-        rightWall.getLocalScale().set(2f, 2f, 75f );
-        rightWall.setLocalTranslation(-5.5f, 0.5f, 2.5f);
-
-        PhysicsBox backWall = staticNode.createBox( "backWall" ); 
-        backWall.getLocalScale().set(13f, 4f, 2f );
-        backWall.setLocalTranslation(0f, 1.5f, 41f);
-        //color (backWall, new ColorRGBA(1f, 1f, 1f, 1f));
-
-        Utils.color(backWall, new ColorRGBA(1f, 1f, 1f, 1f), 1);
-        
-        
+		// Position camera
+		setupCamera();
+		
         // Create Needed objects
+        createLine();
         createPins();
         createBowlingBall();
-        
-        // Position camera
-        setupCamera();
         
         // Set up scene for game
         setupScene();
         
-        /*physicsStepInputHandler = new InputHandler();
-        getPhysicsSpace().addToUpdateCallbacks( new PhysicsUpdateCallback() {
-            public void beforeStep( PhysicsSpace space, float time ) {
-                physicsStepInputHandler.update( time );
-            }
-            public void afterStep( PhysicsSpace space, float time ) {
-
-            }
-        } );
-
-        physicsStepInputHandler.addAction( new MyInputAction( new Vector3f( 0, 0, 700 ) ),
-                InputHandler.DEVICE_KEYBOARD, KeyInput.KEY_O, InputHandler.AXIS_NONE, true );
-        // the action is defined below
-        // we register it to be invoked every update of the input handler while the HOME key (POS1) is down
-        //                              ( last parameter value is 'true' )
-        // note: as the used input handler gets updated each physics step the force is framerate independent -
-        //       we can't use the normal input handler here!
-
-        // register an action for the other direction, too
-        physicsStepInputHandler.addAction( new MyInputAction( new Vector3f( 0, 0, -700 ) ),
-                InputHandler.DEVICE_KEYBOARD, KeyInput.KEY_P, InputHandler.AXIS_NONE, true );
+        input.addAction( new ApplyForceAction(), InputHandler.DEVICE_KEYBOARD, KeyInput.KEY_SPACE, InputHandler.AXIS_NONE, false );
+        input.addAction( new SetForceAction(), InputHandler.DEVICE_KEYBOARD, KeyInput.KEY_F, InputHandler.AXIS_NONE, true );
         
-        // note: we do not move the collision geometry but the physics node!
-
-        // ok we have created some physics stuff but no actual meshes that could be seen
-        // thus we activate the debug mode to allow us to see anything (can be toggled in game with key V)
-         * 
-         * 
-         */
-        
-        this.ball.addForce(new Vector3f( 0, 0, 15000 ));
+        Text label = Text.createDefaultTextLabel( "instructions", "[f] to increase force. [space bar] to throw the ball." );
+        label.setLocalTranslation( 0, 20, 0 );
+        statNode.attachChild( label );
         
         showPhysics = true;
     }
+
+	//TODO: sacar esta clase afuera, meterla en un package aparte
+	private class ApplyForceAction extends InputAction {
+
+        public ApplyForceAction() {
+        }
+
+        public void performAction( InputActionEvent evt ) {
+            if ( evt.getTriggerPressed() ) {
+                // key goes down - apply motion
+            	Bowling.this.ball.addForce( new Vector3f(0, 0, Bowling.this.forceMagnitude) );
+            	
+            	Text label = Text.createDefaultTextLabel( "force", "Force applied: " + Bowling.this.forceMagnitude );
+                label.setLocalTranslation( 0, 5, 0 );
+                statNode.attachChild( label );
+            }
+        }
+    }
 	
-    private void createPins() {
+	//TODO: sacar esta clase afuera, meterla en un package aparte	
+	private class SetForceAction extends InputAction {
+		private static final int FORCE_STEP = 50;
+		private static final int FORCE_MIN = 0;
+		private static final int FORCE_MAX = 20000;
+
+		public SetForceAction( ) {
+			Bowling.this.forceMagnitude = FORCE_MIN;
+        }
+
+        public void performAction( InputActionEvent evt ) {
+            if ( evt.getTriggerAllowsRepeats() && Bowling.this.forceMagnitude < FORCE_MAX) {
+            	Bowling.this.forceMagnitude += FORCE_STEP;
+            }
+        }
+	}	
+	
+    private void createLine() {
+    	StaticPhysicsNode staticNode = getPhysicsSpace().createStaticNode();
+        rootNode.attachChild( staticNode );
+
+        this.lineFloor = staticNode.createBox( "floor");
+
+        this.leftGutter = staticNode.createBox( "leftGutter" ); 
+        this.rightGutter = staticNode.createBox( "rightGutter" ); 
+        this.backGutter = staticNode.createBox( "backGutter" ); 
+        
+        this.leftWall = staticNode.createBox( "leftWall" ); 
+        this.rightWall = staticNode.createBox( "rightWall" ); 
+        this.backWall = staticNode.createBox( "backWall" ); 
+	}
+
+	private void createPins() {
     	
-    	if (null != this.pins) {
+    	if ( null != this.pins ) {
     		return;
     	}
     	
@@ -209,7 +192,6 @@ public class Bowling extends SimplePhysicsGame {
         	this.loadModel(PIN_JME_MODEL_PATH, pin, true);
         	
         	pin.setLocalScale(0.25f);
-//        	pin.setMaterial(createMaterial("pin", 3f, 0.5f, 0.05f));
         	pin.setMaterial(Material.WOOD);
         	pin.computeMass();
         	
@@ -230,6 +212,7 @@ public class Bowling extends SimplePhysicsGame {
         
         // Set the physic properties
         final Sphere sphere = new Sphere("ball-geom", 100, 100, 5f);
+        color(sphere, ColorRGBA.blue);
         this.ball.attachChild(sphere);
         this.ball.setLocalScale(0.1f);
         
@@ -242,36 +225,56 @@ public class Bowling extends SimplePhysicsGame {
 	}
 
 	private void setupCamera() {
-    	
-    	cam.setLocation(new Vector3f (30f, 5f, 0f));
-        cam.lookAt(new Vector3f(0f, 1.5f, -3f), new Vector3f (0, 1, 0));
-        
-        //cam.setLeft(new Vector3f (0f, 0f, 0f));
-        //cam.setDirection(new Vector3f(-5f, 1.5f, -3f));
-        //cam.
+    	cam.setLocation(new Vector3f (10f, 20f, -70f));
+        cam.lookAt(new Vector3f(0f, 1.5f, 41f), new Vector3f (0, 1, 0)); //0, 1.5, -3
 	}
     
     private void setupScene() {
+    	setupLine();
+    	setupPins();
+    	setupBowlingBall();
+	}
+    
+    private void setupLine() {
+    	this.lineFloor.getLocalScale().set( 5, 1f, 70 ); //TODO: ver que material le damos al piso
+
+    	this.leftGutter.getLocalScale().set( 2, 0.6f, 70 );
+    	this.leftGutter.setLocalTranslation(3.5f, -0.2f, 0);
+
+    	this.rightGutter.getLocalScale().set( 2, 0.6f, 70 );
+    	this.rightGutter.setLocalTranslation(-3.5f, -0.2f, 0);
+
+    	this.backGutter.getLocalScale().set( 9, 0.4f, 5f );
+    	this.backGutter.setLocalTranslation(0f, -0.25f, 37.5f);
+
+    	this.leftWall.getLocalScale().set(2f, 2f, 75f );
+    	this.leftWall.setLocalTranslation(5.5f, 0.5f, 2.5f);
+
+    	this.rightWall.getLocalScale().set(2f, 2f, 75f );
+    	this.rightWall.setLocalTranslation(-5.5f, 0.5f, 2.5f);
+
+    	this.backWall.getLocalScale().set(13f, 4f, 2f );
+    	this.backWall.setLocalTranslation(0f, 1.5f, 41f);
+	}
+
+	private void setupPins(){
     	float x[] = {0, -0.5f, 0.5f, -1f, 0, 1f, -1.5f, -0.5f, 0.5f, 1.5f};
-        float z[] = {0, 1f, 1f, 2f, 2f, 2f, 3f, 3f, 3f, 3f};
-        
-        for ( int i = 0; i < PIN_COUNT; i++ ) {
-        	
-        	DynamicPhysicsNode pin = this.pins.get(i);
-        	
-	        pin.setLocalTranslation(x[i] * PIN_DISTANCE, 1.32f, z[i] * PIN_DISTANCE + PIN_OFFSET);
-	    	Quaternion q = new Quaternion();
-	    	q.fromAngles((float) -Math.PI/2, 0, 0);
-	    	pin.setLocalRotation(q);
-        }
-        
-        /*DynamicPhysicsNode ball = (DynamicPhysicsNode) rootNode.getChild("ball") ;
-        ball.setMaterial(Material.PLASTIC);
-        ball.computeMass();
-        color (ball, new ColorRGBA( 1f, 1f, 1f, 0.6f ) );
-        //ball.addForce(new Vector3f(50f, 50f, 0f));*/
-        
-        ball.setLocalTranslation(0f, 1f, -32f);
+    	float z[] = {0, 1f, 1f, 2f, 2f, 2f, 3f, 3f, 3f, 3f};
+
+    	//TODO: tener en cuenta cuando caen los pinos, no se crean/colocan todos
+    	for ( int i = 0; i < PIN_COUNT; i++ ) {
+    		
+    		DynamicPhysicsNode pin = this.pins.get(i);
+    		
+    		pin.setLocalTranslation(x[i] * PIN_DISTANCE, 1.32f, z[i] * PIN_DISTANCE + PIN_OFFSET);
+    		Quaternion q = new Quaternion();
+    		q.fromAngles((float) -Math.PI/2, 0, 0);
+    		pin.setLocalRotation(q);
+    	}
+    }
+
+	private void setupBowlingBall() {
+		this.ball.setLocalTranslation(0f, 1f, -32f);
 	}
 
     private void color( Spatial spatial, ColorRGBA color ) {
@@ -289,15 +292,6 @@ public class Bowling extends SimplePhysicsGame {
         spatial.setRenderState( materialState );
     }
     
-	/**
-     * The main method to allow starting this class as application.
-     *
-     * @param args command line arguments
-     */
-    public static void main( String[] args ) {
-        new Bowling().start();
-    }
-    
     private Material createMaterial(String name, float density, float mu, float bounce) {
     	Material ballMaterial = new Material(name);
     	ballMaterial.setDensity(density);
@@ -307,5 +301,14 @@ public class Bowling extends SimplePhysicsGame {
     	ballMaterial.putContactHandlingDetails( Material.DEFAULT, contactDetails );
 
     	return ballMaterial;
+    }
+
+    /**
+     * The main method to allow starting this class as application.
+     *
+     * @param args command line arguments
+     */
+    public static void main( String[] args ) {
+    	new Bowling().start();
     }
 }
