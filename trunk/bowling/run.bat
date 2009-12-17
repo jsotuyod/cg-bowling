1ce16daa1e08b532efe3e9e82002b1b1e42b4f43
aboldt@gmail.com <aboldt@gmail.com@298503bc-7942-11de-bf68-bf76f4d3b5b5>
2009-12-17 11:03:05 +0000

    echo off al principio y cd .. al final

--- trunk/bowling/run.bat
+++ trunk/bowling/run.bat
@@ -1,2 +1,5 @@
+@echo off
+
 cd build
 java -Djava.library.path=.\lib\native\windows -classpath .\bin;.\lib\jme.jar;.\lib\jmephysics2.jar;.\lib\gluegen-rt.jar;.\lib\jogl.jar;.\lib\jorbis-0.0.17.jar;.\lib\gluegen-rt.jar;.\lib\jogl.jar;.\lib\jorbis-0.0.17.jar;.\lib\jinput.jar;.\lib\lwjgl_util_applet.jar;.\lib\lwjgl_util.jar;.\lib\lwjgl.jar;.\lib\jme-colladabinding.jar;.\lib\swt_windows.jar;.\lib\odejava-jni.jar bowling.main.Bowling
+cd ..
